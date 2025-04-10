1b937b688382f2e8b90266ca8cdea18df6349870
Gomdadi <35wnsgh@naver.com>
2025-04-10 13:17:02 +0900

    feat: RESTful API 라우팅 경로 설정 - /api/auth/login 로그인 - /api/auth/logout 로그아웃 - /api/auth/reissue jwt 토큰 재발급 - /api/protected/* 미들웨어 추가 - 사용자 인증이 필요한 API 요청의 경우, jwt.guard.ts 미들웨어를 거쳐서 인증 작업을 마쳐야함

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Express } from 'express';
-// e.g.,
-// import loginRoutes from './routes/loginRoutes';
+import authRoutes from './route/auth.routes';
+import jwtGuard from "./middleware/jwt.guard";
+import cookieParser from 'cookie-parser';
 
 const app : Express = express();
 app.use(express.json());
+app.use(cookieParser());
 
-// e.g.,
-// app.use('/api/login', loginRoutes);
+// '/api/protected/*'이면 인증 미들웨어를 거침
+app.use('/api/protected', jwtGuard);
+
+// '/api/auth/*'(/login, /logout, /reissue)의 경우 인증이 불필요하기에 제외
+app.use('/api/auth', authRoutes);
 
 export default app;
