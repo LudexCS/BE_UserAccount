075ff42a08d23ec1d9e164ac1241f3fbdb0a1896
Gomdadi <35wnsgh@naver.com>
2025-04-12 01:16:53 +0900

    feat: db 연결 및 account 엔티티, 레포지토리 생성

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import dotenv from 'dotenv';
 import app from './app';
+import AppDataSource from './config/mysql.config';
 
 // .env 환경변수 사용 setting
 dotenv.config();
 
-const PORT : string|3000 = process.env.PORT || 3000;
-app.listen(PORT, () : void => {
-    console.log(`UserAccount service running on port ${PORT}`);
-});
+const PORT = process.env.PORT;
+
+// DB 연결
+AppDataSource.initialize()
+    .then((): void => {
+        console.log('📦 DB connected');
+        // 서버 실행
+        app.listen(PORT, () : void => {
+            console.log(`UserAccount service running on port ${PORT}`);
+        });
+    })
+    .catch((error) => console.error('DB 연결 실패:', error));
+
