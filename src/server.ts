da239d9d61e3895e70769eb4c1e3376f0adf9185
Gomdadi <35wnsgh@naver.com>
2025-04-13 21:48:03 +0900

    feat: db connection 실패 시 재시도 로직 추가

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,34 @@
 import dotenv from 'dotenv';
 import app from './app';
 import AppDataSource from './config/mysql.config';
+import { startGrpcServer } from './grpc/auth.server';
 
-// .env 환경변수 사용 setting
 dotenv.config();
-
 const PORT = process.env.PORT;
 
-// DB 연결
-AppDataSource.initialize()
-    .then((): void => {
-        console.log('📦 DB connected');
-        // 서버 실행
-        app.listen(PORT, () : void => {
-            console.log(`UserAccount service running on port ${PORT}`);
-        });
-    })
-    .catch((error) => console.error('DB 연결 실패:', error));
+async function connectAndStart() {
+    let retries = 5;
+    while (retries--) {
+        try {
+            await AppDataSource.initialize();
+            console.log('📦 DB connected');
 
+            await startGrpcServer();
+
+            app.listen(PORT, () => {
+                console.log(`UserAccount REST API running on port ${PORT}`);
+            });
+            break;
+        } catch (error) {
+            console.error('❌ DB 연결 실패:', error);
+            if (retries === 0) {
+                console.error('⛔️ 재시도 모두 실패. 앱 종료.');
+                process.exit(1);
+            }
+            console.log('⏳ 3초 후 재시도...');
+            await new Promise((res) => setTimeout(res, 3000));
+        }
+    }
+}
+
+connectAndStart();
