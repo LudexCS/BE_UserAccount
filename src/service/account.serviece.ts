c18d2ee22cf2d577ee464706ddb1e1473a80e60f
jinoo <gart09@naver.com>
2025-04-13 14:33:32 +0900

    refactor: 계정삭제 코드 변경
    account.service 내 db직접 연결 코드 삭제 안한 부분 삭제

--- src/service/account.serviece.ts
+++ src/service/account.serviece.ts
@@ -2,23 +2,6 @@ import AppDataSource from '../config/mysql.config;
 import { Account } from '../entity/account.entity';
 import { deleteAccountByEmail } from '../repositories/account.repository';
 
-export const deleteUserAccount = async (userId: number, email: string): Promise<void> => {
-    const accountRepo = AppDataSource.getRepository(Account);
-
-    const user = await accountRepo.findOne({ where: { id: userId }});
-
-    if (!user) {
-        throw new Error('사용자를 찾을 수 없습니다.');
-    }
-
-    if (user.email !== email) {
-        throw new Error('이메일이 일치하지 않습니다.');
-    }
-
-    // 계정 삭제
-    await accountRepo.delete({ id: userId });
-};
-
 export async function deleteAccount(email: string): Promise<void> {
     await deleteAccountByEmail(email);
 }
