ef97020b1b9e9cee9dd5132e4e6f4171668b36e9
Gomdadi <35wnsgh@naver.com>
2025-06-03 23:28:54 +0900

    feat: Update CORS configuration to support additional origins
    Expanded the allowed origins in the CORS configuration to include `uosludex.com` and its HTTPS variant.

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app : Express = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://uosludex.com', 'https://uosludex.com'],
     credentials: true,
     exposedHeaders: ["authorization", "set-cookie"]
 }));
