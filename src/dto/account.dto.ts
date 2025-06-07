887222ec23c997717fc83fa28574efdd86e270c1
Gomdadi <35wnsgh@naver.com>
2025-06-08 02:36:37 +0900

    feat: Add `ownerId` field to AccountDto and update service logic
    Extended AccountDto with the `ownerId` field and updated `toAccountDto` and associated service methods to handle the new parameter.

--- src/dto/account.dto.ts
+++ src/dto/account.dto.ts
@@ -3,13 +3,15 @@ import {CryptoWalletDto} from "./cryptoWallet.dto";
 export interface AccountDto {
     email: string;
     nickname: string;
+    ownerId: string;
     cryptoWallet: CryptoWalletDto[];
 }
 
-export const toAccountDto = ( email: string, nickname: string, cryptoWallet: CryptoWalletDto[]): AccountDto => {
+export const toAccountDto = ( email: string, nickname: string, ownerId: string, cryptoWallet: CryptoWalletDto[]): AccountDto => {
     return {
         email: email,
         nickname: nickname,
+        ownerId: ownerId,
         cryptoWallet: cryptoWallet
     }
 }
